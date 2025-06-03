9609b922323699e76d87eeac370ab8742df9b4f4
Harmish001 <Harmish001@users.noreply.github.com>
2025-06-03 12:59:32 +0000

    deploy: f267d5c4284fd838ce15e8424cce797a055c1f24

---
+++ assets/index-BfT8b5-s.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-DoINAaG_.js","assets/index--VboDSnl.js","assets/index-BLaVQrYx.css"])))=>i.map(i=>d[i]);
+import{r as p,_ as r}from"./index--VboDSnl.js";const _=p("App",{web:()=>r(()=>import("./web-DoINAaG_.js"),__vite__mapDeps([0,1,2])).then(e=>new e.AppWeb)});export{_ as App};

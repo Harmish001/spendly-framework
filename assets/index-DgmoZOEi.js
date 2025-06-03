056152a83dba0572ad1466df206b85c4425a8470
Harmish001 <Harmish001@users.noreply.github.com>
2025-06-03 13:24:35 +0000

    deploy: abaa62ba052cec5ec53d47a734d8ae8cc86165e0

---
+++ assets/index-DgmoZOEi.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-p7sDH637.js","assets/index-DTugnwsh.js","assets/index-BLaVQrYx.css"])))=>i.map(i=>d[i]);
+import{r as p,_ as r}from"./index-DTugnwsh.js";const _=p("App",{web:()=>r(()=>import("./web-p7sDH637.js"),__vite__mapDeps([0,1,2])).then(e=>new e.AppWeb)});export{_ as App};

b31706e3712d86efb3a2815d42b4704be1ec2b59
Harmish001 <Harmish001@users.noreply.github.com>
2025-06-11 03:17:49 +0000

    deploy: 6540c80026748de57318ef7bd4935d3ecb336e8c

---
+++ assets/index-CVpEXIs3.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-B3tO-5AZ.js","assets/index-Q_qV5c9Y.js","assets/index-BbZ3CfGJ.css"])))=>i.map(i=>d[i]);
+import{r as p,_ as r}from"./index-Q_qV5c9Y.js";const _=p("App",{web:()=>r(()=>import("./web-B3tO-5AZ.js"),__vite__mapDeps([0,1,2])).then(e=>new e.AppWeb)});export{_ as App};

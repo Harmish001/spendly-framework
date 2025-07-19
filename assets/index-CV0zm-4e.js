241425d1410a95078c7c0b010aff8912980f1af6
Harmish001 <Harmish001@users.noreply.github.com>
2025-07-19 08:13:45 +0000

    deploy: 45f87586577d464c8b42b92db19b117e10a0faf6

---
+++ assets/index-CV0zm-4e.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-Cqa3wyoH.js","assets/index-B7YpGCJ3.js","assets/index-CNg-2wbt.css"])))=>i.map(i=>d[i]);
+import{r as p,_ as r}from"./index-B7YpGCJ3.js";const _=p("App",{web:()=>r(()=>import("./web-Cqa3wyoH.js"),__vite__mapDeps([0,1,2])).then(e=>new e.AppWeb)});export{_ as App};

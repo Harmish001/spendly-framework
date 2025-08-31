6627d1983342e40a018886da21012bacf0b2f514
Harmish001 <Harmish001@users.noreply.github.com>
2025-08-31 07:23:25 +0000

    deploy: 3e83c19da83900ba1fae99048d5317a2f52c4f07

---
+++ assets/index-Bz5dQHLo.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-B_Pi5nOv.js","assets/index-CjubBkmj.js","assets/index-DZPZ-Wsx.css"])))=>i.map(i=>d[i]);
+import{r as p,_ as r}from"./index-CjubBkmj.js";const _=p("App",{web:()=>r(()=>import("./web-B_Pi5nOv.js"),__vite__mapDeps([0,1,2])).then(e=>new e.AppWeb)});export{_ as App};
